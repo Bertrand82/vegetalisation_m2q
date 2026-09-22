b1f1f5455fcf89944919721701937a2b2178cc21
Bertrand82 <bertrand.guiral@gmail.com>
2026-09-22 08:08:01 +0200

    s

---
+++ build.bat
@@ -0,0 +1,6 @@
+pandoc presentation.md -o presentation.html
+pandoc presentation.md -o presentation.pptx
+pandoc convention.md -o convention.pdf
+pandoc convention.md -o convention.html
+git add .
+git commit -m "s"
